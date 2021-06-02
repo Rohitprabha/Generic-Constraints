87d1fbf30fd7cb8c12805f63397d59df7da4bdbf
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 19:33:53 +0530

    Add files via upload

---
+++ Example 4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Generic_Constraints_4
+{
+    public class Sample<T> where T : new()
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample<ClassA> obj1 = new Sample<ClassA>(); //No Error, as ClassA has constructor of no parameters.
+
+            //Sample<ClassB> obj2 = new Sample<ClassB>(); //Error, as ClassB has constructor which takes parameters.
+
+        }
+    }
+    public class ClassA
+    {
+        public ClassA()
+        {
+
+        }
+    }
+
+    public class ClassB
+    {
+        public ClassB(string customerName)
+        {
+
+        }
+    }
+}
