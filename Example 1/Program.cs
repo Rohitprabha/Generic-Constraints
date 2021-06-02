11c60065ded9e3259fa3f27554e06c0d7a7e3db3
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 19:29:10 +0530

    Add files via upload

---
+++ Example 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericConstraints_Example_1
+{
+    public class GenericClass<T> where T : class
+    {
+        private T Name;
+        private T location;
+        public void genericMethod(T Name, T location)
+        {
+            this.Name = Name;
+            this.location = location;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Location: {0}", location);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> obj = new GenericClass<string>();
+            obj.genericMethod("Rohit", "Bhimavaram");
+            obj.Display();
+        }
+    }
+}
