b0f9758445170504613d2695ffc3fc74db82a4eb
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 19:30:43 +0530

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericConstraints_Example_2
+{
+    public class GenericClass<T> where T : struct
+    {
+        private T ID;
+        private T Phno;
+        public void genericMethod(T ID, T Phno)
+        {
+            this.ID = ID;
+            this.Phno = Phno;
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Phone Number: {0}", Phno);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<long> obj = new GenericClass<long>();
+            obj.genericMethod(107, 9703002617);
+            obj.Display();
+        }
+    }
+}
